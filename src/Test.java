0e2990f42c59dd2e5203867f264155b1d0d39b25
sankeerth <sankeerthp59@gmail.com>
2021-02-19 18:49:26 +0530

    this is my first commit

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry= "yes";
+		if(hungry == "yes")
+			System.out.println("eating something");
+		else {
+			System.out.println(" doing work");
+		}
+
+	}
+
+}
